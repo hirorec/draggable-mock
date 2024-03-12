af67487a26dea0930b6a1db630d62a0ed3bcec3f
hiroaki kitamura <kitamura@start-rec.net>
2024-03-12 10:49:33 +0900

    const

---
+++ src/const/boxList.ts
@@ -0,0 +1,164 @@
+import { BoxProps } from '@/features/BoxApplication/types';
+
+export const BOX_LIST: BoxProps[] = [
+  {
+    id: '1-1',
+    colIndex: 0,
+    backgroundColor: '#FFE4BF',
+    borderColor: '#F2A455',
+    label: `Box1-1`,
+    position: {
+      x: 0,
+      y: 0,
+    },
+    localPosition: {
+      x: 0,
+      y: 0,
+    },
+    size: {
+      width: 1,
+      height: 2,
+    },
+    zIndex: 0,
+  },
+  {
+    id: '1-2',
+    colIndex: 0,
+    backgroundColor: '#FFE4BF',
+    borderColor: '#F2A455',
+    label: `Box1-2`,
+    position: {
+      x: 0,
+      y: 2,
+    },
+    localPosition: {
+      x: 0,
+      y: 0,
+    },
+    size: {
+      width: 1,
+      height: 3,
+    },
+    zIndex: 0,
+  },
+  {
+    id: '1-3',
+    colIndex: 0,
+    backgroundColor: '#FFE4BF',
+    borderColor: '#F2A455',
+    label: `Box1-3`,
+    position: {
+      x: 0,
+      y: 5,
+    },
+    localPosition: {
+      x: 0,
+      y: 0,
+    },
+    size: {
+      width: 1,
+      height: 3,
+    },
+    zIndex: 0,
+  },
+  {
+    id: '2-0',
+    colIndex: 1,
+    backgroundColor: '#E6F7DA',
+    borderColor: '#93ED6F',
+    label: `Box2-0`,
+    position: {
+      x: 1,
+      y: 0,
+    },
+    localPosition: {
+      x: 0,
+      y: 0,
+    },
+    size: {
+      width: 1,
+      height: 3,
+    },
+    zIndex: 0,
+  },
+  {
+    id: '2-1',
+    colIndex: 1,
+    backgroundColor: '#E6F7DA',
+    borderColor: '#93ED6F',
+    label: `Box2-1`,
+    position: {
+      x: 1,
+      y: 3,
+    },
+    localPosition: {
+      x: 0,
+      y: 0,
+    },
+    size: {
+      width: 1,
+      height: 4,
+    },
+    zIndex: 0,
+  },
+  {
+    id: '2-2',
+    colIndex: 1,
+    backgroundColor: '#E6F7DA',
+    borderColor: '#93ED6F',
+    label: `Box2-2`,
+    position: {
+      x: 1,
+      y: 7,
+    },
+    localPosition: {
+      x: 0,
+      y: 0,
+    },
+    size: {
+      width: 1,
+      height: 5,
+    },
+    zIndex: 0,
+  },
+  {
+    id: '4',
+    colIndex: 2,
+    backgroundColor: '#FFE4BF',
+    borderColor: '#F2A455',
+    label: `Box4`,
+    position: {
+      x: 2,
+      y: 0,
+    },
+    localPosition: {
+      x: 0,
+      y: 0,
+    },
+    size: {
+      width: 1,
+      height: 15,
+    },
+    zIndex: 0,
+  },
+  {
+    id: '5',
+    colIndex: 3,
+    backgroundColor: '#FFE4BF',
+    borderColor: '#F2A455',
+    label: `Box5`,
+    position: {
+      x: 3,
+      y: 1,
+    },
+    localPosition: {
+      x: 0,
+      y: 0,
+    },
+    size: {
+      width: 1,
+      height: 5,
+    },
+    zIndex: 0,
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import _ from 'lodash';
 import React, { useCallback, useEffect, useState } from 'react';
 
+import { BOX_LIST } from '@/const/boxList';
 import { BoxApplication } from '@/features/BoxApplication';
 import { BoxProps, ColumnProps } from '@/features/BoxApplication/types';
 
@@ -13,169 +14,7 @@ export default function Page() {
   const rowDiv = 30;
 
   useEffect(() => {
-    const boxList: BoxProps[] = [
-      {
-        id: '1-1',
-        colIndex: 0,
-        backgroundColor: '#FFE4BF',
-        borderColor: '#F2A455',
-        label: `Box1-1`,
-        position: {
-          x: 0,
-          y: 0,
-        },
-        localPosition: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 1,
-          height: 2,
-        },
-        zIndex: 0,
-      },
-      {
-        id: '1-2',
-        colIndex: 0,
-        backgroundColor: '#FFE4BF',
-        borderColor: '#F2A455',
-        label: `Box1-2`,
-        position: {
-          x: 0,
-          y: 2,
-        },
-        localPosition: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 1,
-          height: 3,
-        },
-        zIndex: 0,
-      },
-      {
-        id: '1-3',
-        colIndex: 0,
-        backgroundColor: '#FFE4BF',
-        borderColor: '#F2A455',
-        label: `Box1-3`,
-        position: {
-          x: 0,
-          y: 5,
-        },
-        localPosition: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 1,
-          height: 3,
-        },
-        zIndex: 0,
-      },
-      {
-        id: '2-0',
-        colIndex: 1,
-        backgroundColor: '#E6F7DA',
-        borderColor: '#93ED6F',
-        label: `Box2-0`,
-        position: {
-          x: 1,
-          y: 0,
-        },
-        localPosition: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 1,
-          height: 3,
-        },
-        zIndex: 0,
-      },
-      {
-        id: '2-1',
-        colIndex: 1,
-        backgroundColor: '#E6F7DA',
-        borderColor: '#93ED6F',
-        label: `Box2-1`,
-        position: {
-          x: 1,
-          y: 3,
-        },
-        localPosition: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 1,
-          height: 4,
-        },
-        zIndex: 0,
-      },
-      {
-        id: '2-2',
-        colIndex: 1,
-        backgroundColor: '#E6F7DA',
-        borderColor: '#93ED6F',
-        label: `Box2-2`,
-        position: {
-          x: 1,
-          y: 7,
-        },
-        localPosition: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 1,
-          height: 5,
-        },
-        zIndex: 0,
-      },
-      {
-        id: '4',
-        colIndex: 2,
-        backgroundColor: '#FFE4BF',
-        borderColor: '#F2A455',
-        label: `Box4`,
-        position: {
-          x: 2,
-          y: 0,
-        },
-        localPosition: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 1,
-          height: 15,
-        },
-        zIndex: 0,
-      },
-      {
-        id: '5',
-        colIndex: 3,
-        backgroundColor: '#FFE4BF',
-        borderColor: '#F2A455',
-        label: `Box5`,
-        position: {
-          x: 3,
-          y: 1,
-        },
-        localPosition: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 1,
-          height: 5,
-        },
-        zIndex: 0,
-      },
-    ];
-    setBoxList(boxList);
+    setBoxList(BOX_LIST);
   }, []);
 
   useEffect(() => {
@@ -185,10 +24,10 @@ export default function Page() {
       { id: '3', label: 'column 3', rowDiv, colDiv: 1 },
       { id: '4', label: 'column 4', rowDiv, colDiv: 1 },
       { id: '5', label: 'column 5', rowDiv, colDiv: 1 },
-      // { id: '6', label: 'column 6', rowDiv, colDiv: 1 },
-      // { id: '7', label: 'column 7', rowDiv, colDiv: 1 },
-      // { id: '8', label: 'column 8', rowDiv, colDiv: 1 },
-      // { id: '9', label: 'column 9', rowDiv, colDiv: 1 },
+      { id: '6', label: 'column 6', rowDiv, colDiv: 1 },
+      { id: '7', label: 'column 7', rowDiv, colDiv: 1 },
+      { id: '8', label: 'column 8', rowDiv, colDiv: 1 },
+      { id: '9', label: 'column 9', rowDiv, colDiv: 1 },
     ];
     setColumnList(columnList);
   }, []);
