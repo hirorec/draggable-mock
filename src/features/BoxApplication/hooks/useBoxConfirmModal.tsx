b1f3e80cfa3f87a1c3753f0b9c14070d2a970670
hiroaki kitamura <kitamura@start-rec.net>
2024-03-25 19:15:55 +0900

    no message

--- src/features/BoxApplication/hooks/useBoxConfirmModal.tsx
+++ src/features/BoxApplication/hooks/useBoxConfirmModal.tsx
@@ -1,16 +1,46 @@
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext, useCallback } from 'react';
 
 export type BoxConfirmModalContextType = {
   isOpen: boolean;
   setIsOpen: (value: boolean) => void;
+  showModal: (approveCallBack: () => void, rejectCallback: () => void) => void;
+  onApprove: () => void;
+  onReject: () => void;
 } | null;
 
 export const useBoxConfirmModalOrigin = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [approveCallBack, setApproveCallBack] = useState<() => void>();
+  const [rejectCallback, setRejectCallback] = useState<() => void>();
+
+  const showModal = (approveCallBack: () => void, rejectCallback: () => void) => {
+    setApproveCallBack(approveCallBack);
+    setRejectCallback(rejectCallback);
+    setIsOpen(true);
+  };
+
+  const onApprove = useCallback(() => {
+    if (approveCallBack) {
+      approveCallBack();
+    }
+
+    setIsOpen(false);
+  }, []);
+
+  const onReject = useCallback(() => {
+    if (rejectCallback) {
+      rejectCallback();
+    }
+
+    setIsOpen(false);
+  }, []);
 
   return {
     isOpen,
     setIsOpen,
+    showModal,
+    onApprove,
+    onReject,
   };
 };
 
